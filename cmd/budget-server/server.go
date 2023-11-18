d05add89004b4701ba6ac0e93e23fcceefc5be04
Romelo Gilbert <romelogilbert@yahoo.com>
2023-11-17 23:53:29 -0500

    docker configured for api server & mysql db

--- cmd/budget-server/server.go
+++ cmd/budget-server/server.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	db2 "github.com/Seymour-creates/budget-server/internal/db"
 	"github.com/Seymour-creates/budget-server/internal/router"
-	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv"
 	"log"
 	"os"
@@ -12,19 +9,12 @@ import (
 
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Print("No dev.env file found")
-	}
-
-	dsn := os.Getenv("DSN")
-	fmt.Println("Data source name? :", dsn)
-	if err := db2.InitDB(dsn); err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
-	}
 	port := os.Getenv("PORT")
-
 	srv := router.NewServer()
+
 	if err := srv.Run(port); err != nil {
-		log.Fatal("Server failed to start:", err)
+		log.Fatal("Server failed to start: ", err)
 	}
+
+	log.Printf("Server running on port %v", port)
 }
